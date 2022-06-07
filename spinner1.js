03f90e6ca5de40e90472c9b14f4762c5b85293f8
josemourinho333 <yoo.phil92@gmail.com>
2022-06-07 09:27:35 -0700

    low fidelity loading spinner with \r and setTimeout()

---
+++ spinner1.js
@@ -0,0 +1,24 @@
+setTimeout(() => {
+  process.stdout.write('\r|    ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/    ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-    ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\    ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|    ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, 900);
+
